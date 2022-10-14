b7f34399f366780f8873a62fd5fd36afc1638ac4
pablo vargas <pablo.vargas.js@gmail.com>
2022-10-14 15:16:07 -0400

    crecion

---
+++ src/shared/dto/pagination.dto.ts
@@ -0,0 +1,16 @@
+import { IsNumber, IsPositive, IsOptional } from 'class-validator';
+
+export class PaginationDto {
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    inicio: number;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    pageSize: number;
+
+    @IsOptional()
+    text?:string
+}
