32201b90a5e6bf35e34dddf67fc2b7ada555873c
Marco Castro <marco.castro@alimx.mx>
2023-05-12 18:12:03 -0600

    Carga Cliente

--- src/modules/application/application.controller.ts
+++ src/modules/application/application.controller.ts
@@ -4,12 +4,19 @@ import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('application')
 export class ApplicationController {
-    constructor(
-        private readonly service: ApplicationService
-    ) { }
-    //---------------------------------------------------------------------------------------------
-    @MessagePattern({ cmd: 'validBlacklist' })
-    async validBlacklist(validBlacklist: number) {
-        return await this.service.validBlacklist(validBlacklist);
-    }
+  constructor(private readonly service: ApplicationService) {}
+  //---------------------------------------------------------------------------------------------
+  @MessagePattern({ cmd: 'validBlacklist' })
+  async validBlacklist(validBlacklist: number) {
+    return await this.service.validBlacklist(validBlacklist);
+  }
+
+  @MessagePattern({ cmd: 'cargaCliente1' })
+  async cargaCliente1(no_nombramiento: number) {
+    return await this.service.cargaCliente1(no_nombramiento);
+  }
+  @MessagePattern({ cmd: 'cargaCliente2' })
+  async cargaCliente2(no_nombramiento: number) {
+    return await this.service.cargaCliente2(no_nombramiento);
+  }
 }
