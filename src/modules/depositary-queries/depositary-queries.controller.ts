519845a6f4b540f97e1d5bb46f39efd027e27f11
Alex Lujan <alexlujanm@outlook.com>
2023-06-07 10:16:51 -0600

    Added endpoints /api/v1/depositary-queries/pagorefDepositariasRatificar?importe=1010293&aux_refe1=031582015013433320 /api/v1/depositary-queries/pagorefDepositariasAuxRefe?aux_refe1=031582015013433320

--- src/modules/depositary-queries/depositary-queries.controller.ts
+++ src/modules/depositary-queries/depositary-queries.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import {
-    ApiBody,
-    ApiCreatedResponse,
-    ApiOperation,
-    ApiParam,
-    ApiQuery,
-    ApiResponse,
-    ApiTags,
-  } from '@nestjs/swagger';
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Paginate, PaginateQuery } from 'nestjs-paginate';
 import { DepositaryQueriesService } from './depositary-queries.service';
 import { MessagePattern } from '@nestjs/microservices';
@@ -15,54 +15,74 @@ import { GetByParamMapperMenajeDto } from './dto/get-by-param-mapper-menaje.dto'
 import { mapperPadepositaryAssets2Dto } from './dto/mapper-pa-depositary-assets2.dto';
 @Controller('depositary-queries')
 export class DepositaryQueriesController {
-    constructor(private readonly service: DepositaryQueriesService) {}
-    /*@ApiOperation({ summary: 'Paginación de todos los registros' })
-    @ApiResponse({
-      status: 200
-    })
-    @ApiQuery({
-      name: 'page',
-      description: 'Número de página',
-      type: Number,
-      required: false,
-    })
-    @ApiQuery({
-      name: 'limit',
-      description: 'Limite de elementos',
-      type: Number,
-      required: false,
-    })
-    @ApiQuery({
-      name: 'search',
-      description: 'Texto a buscar',
-      type: String,
-      required: false,
-    })
-    @Get()*/
-    @MessagePattern({ cmd: 'GetByParamMappervSSSTipoBien' })
-    async GetByParamMappervSSSTipoBien(query: PaginateQuery) {
-      return await this.service.GetByParamMappervSSSTipoBien(query);
-    }
+  constructor(private readonly service: DepositaryQueriesService) {}
+  /*@ApiOperation({ summary: 'Paginación de todos los registros' })
+  @ApiResponse({
+    status: 200
+  })
+  @ApiQuery({
+    name: 'page',
+    description: 'Número de página',
+    type: Number,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'limit',
+    description: 'Limite de elementos',
+    type: Number,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'search',
+    description: 'Texto a buscar',
+    type: String,
+    required: false,
+  })
+  @Get()*/
+  @MessagePattern({ cmd: 'GetByParamMappervSSSTipoBien' })
+  async GetByParamMappervSSSTipoBien(query: PaginateQuery) {
+    return await this.service.GetByParamMappervSSSTipoBien(query);
+  }
 
-    @MessagePattern({ cmd: 'GetByParamMapperMenaje' })
-    async GetByParamMapperMenaje(dto: GetByParamMapperMenajeDto) {
-      return await this.service.GetByParamMapperMenaje(dto);
-    }
+  @MessagePattern({ cmd: 'GetByParamMapperMenaje' })
+  async GetByParamMapperMenaje(dto: GetByParamMapperMenajeDto) {
+    return await this.service.GetByParamMapperMenaje(dto);
+  }
 
-    @MessagePattern({ cmd: 'querysOracleCatFed' })
-    async querysOracleCatFed() {
-      return await this.service.querysOracleCatFed();
-    }
+  @MessagePattern({ cmd: 'querysOracleCatFed' })
+  async querysOracleCatFed() {
+    return await this.service.querysOracleCatFed();
+  }
 
-    @MessagePattern({ cmd: 'mapperPadepositaryAssets1' })
-    async mapperPadepositaryAssets1(dto: GetByParamMapperMenajeDto) {
-      return await this.service.mapperPadepositaryAssets1(dto);
-    }
+  @MessagePattern({ cmd: 'mapperPadepositaryAssets1' })
+  async mapperPadepositaryAssets1(dto: GetByParamMapperMenajeDto) {
+    return await this.service.mapperPadepositaryAssets1(dto);
+  }
 
-    @MessagePattern({ cmd: 'mapperPadepositaryAssets2' })
-    async mapperPadepositaryAssets2(dto: mapperPadepositaryAssets2Dto) {
-      return await this.service.mapperPadepositaryAssets2(dto);
-    }
+  @MessagePattern({ cmd: 'mapperPadepositaryAssets2' })
+  async mapperPadepositaryAssets2(dto: mapperPadepositaryAssets2Dto) {
+    return await this.service.mapperPadepositaryAssets2(dto);
+  }
 
-    
+  @MessagePattern({ cmd: 'pagorefDepositariasRatificar' })
+  async pagorefDepositariasRatificar({
+    importe,
+    aux_refe1,
+  }: {
+    importe: number;
+    aux_refe1: string;
+  }) {
+    // PAGOREF_DEPOSITARIAS_RATIFICAR
+    return await this.service.pagorefDepositariasRatificar({
+      importe,
+      aux_refe1,
+    });
+  }
+
+  @MessagePattern({ cmd: 'pagorefDepositariasAuxRefe' })
+  async pagorefDepositariasAuxRefe({ aux_refe1 }: { aux_refe1: string }) {
+    return await this.service.pagorefDepositariasAuxRefe({
+      aux_refe1,
+    });
+  }
 }
