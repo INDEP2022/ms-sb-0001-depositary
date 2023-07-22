bceefe1abaaa36eeb3ac22e79965d40b2f57e594
Marco Castro <marco.castro@alimx.mx>
2023-07-21 18:36:01 -0600

    added module goodsquerydbo

--- src/modules/application/application.module.ts
+++ src/modules/application/application.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { ApplicationController } from './application.controller';
 import { ApplicationService } from './application.service';
 import { CommonFilterQueryService } from 'src/shared/service/common-filter-query.service';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Module({
   controllers: [ApplicationController],
-  providers: [ApplicationService, CommonFilterQueryService]
+  providers: [ApplicationService, CommonFilterQueryService, {
+    provide: 'ms-sb-0001-goodsquerydbo',
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) =>
+        ClientProxyFactory.create({
+            transport: Transport.TCP,
+            options: {
+                host: configService.get('GOODSQUERYDBO_HOST_NAME'),
+                port: configService.get('GOODSQUERYDBO_HOST_PORT'),
+            },
+        }),
+}]
 })
 export class ApplicationModule {}
